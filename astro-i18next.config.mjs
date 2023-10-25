6ea42d9681b8f8454405d30933cd09985cb772e6
Christopher Gonzalez <christopher53552@gmail.com>
2023-10-25 00:52:32 -0600

    add multiple language feature

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    routes: {
+      es: {
+        projects: "proyectos",
+        "about-me": "sobre-mi",
+      },
+    },
+  };
